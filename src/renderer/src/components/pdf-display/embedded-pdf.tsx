923c356b8d2125ea9ee6f0fc3d05a09c32200a2a
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-03-21 20:12:48 -0700

    Added a visual indicator to show if a page is selected

--- src/renderer/src/components/pdf-display/embedded-pdf.tsx
+++ src/renderer/src/components/pdf-display/embedded-pdf.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./styles/embedded-pdf.module.css";
 
 interface EmbeddedPDFProps {
@@ -7,16 +8,17 @@ interface EmbeddedPDFProps {
 }
 
 export function EmbeddedPDF({ pdfSRC, index, addPageToArray }: EmbeddedPDFProps) {
-
+  const [isPageInArray, setIsPageInArray] = useState<boolean>(false);
 
   function handleClick() {
     addPageToArray(index);
+    setIsPageInArray(!isPageInArray);
   }
 
   return (
     <div className={styles.mainContainer}>
       <iframe src={`${pdfSRC}#toolbar=0`} className={styles.pdfPage} />
-      <div onClick={handleClick} className={`${styles.interactiveDiv} ${styles.unselectedDiv}`}>{index + 1}</div>
+      <div onClick={handleClick} className={`${styles.interactiveDiv} ${isPageInArray ? styles.selectedDiv : styles.unselectedDiv}`}>{index + 1}</div>
     </div>
   );
 }
